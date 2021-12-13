764bc92f59e1e5232a8b3364dff963d732ca27a7
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-14 00:56:28 +0530

    CREATE: back to cats link added

--- pages/cats/[id].js
+++ pages/cats/[id].js
@@ -21,16 +21,23 @@ const Cat = () => {
 	}, [id]);
 
 	return cat ? (
-		<div className="container d-flex justify-content-center mt-5 align-items-center">
-			<Head>
-				<title>Cats | {cat.name}</title>
-			</Head>
-			<Image src={cat.image.url} alt={cat.image.alt} width={1600} height={2400} />
-			<div className="mx-5">
-				<h1>{cat.name}</h1>
-				<p>{cat.description}</p>
+		<>
+			<div className="container d-flex justify-content-center mt-5 align-items-center">
+				<Head>
+					<title>Cats | {cat.name}</title>
+				</Head>
+				<Image src={cat.image.url} alt={cat.image.alt} width={1600} height={2400} />
+				<div className="mx-5">
+					<h1>{cat.name}</h1>
+					<p>{cat.description}</p>
+				</div>
 			</div>
-		</div>
+			<div className="text-center mt-5">
+				<button type="button" className="btn btn-primary" onClick={() => router.push("/cats")}>
+					Back to Cats
+				</button>
+			</div>
+		</>
 	) : (
 		"Loading..."
 	);
