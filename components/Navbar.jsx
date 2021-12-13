c5004c77035946dce6ad5f0f14730a69cb5c5986
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2021-12-13 20:40:39 +0530

    CREATE: navbar created

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-md">
+				<a className="navbar-brand" href="#">
+					Navbar
+				</a>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
